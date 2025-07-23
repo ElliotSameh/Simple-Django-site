b0efebf0440b9fda8ae246b6739b86eaf1fb63b5
ElliotSameh <elliotsameh@gmail.com>
2025-07-23 18:29:53 -0400

    Adding User Specific Page/Access

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -9,25 +9,27 @@ from .forms import CreateNewList
 def index(request, id):
     ls = ToDoList.objects.get(id=id)
 
-    if request.method == "POST":
-        print(request.POST)
-        if request.POST.get("save"):
-            for item in ls.item_set.all():
-                if request.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in request.user.todolist.all():
+        if request.method == "POST":
+            print(request.POST)
+            if request.POST.get("save"):
+                for item in ls.item_set.all():
+                    if request.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+
+                    item.save()
+            elif request.POST.get("newItem"):
+                txt = request.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
+                    print("Invalid")
 
-                item.save()
-        elif request.POST.get("newItem"):
-            txt = request.POST.get("new")
-
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("Invalid")
-
-    return render(request, "main/list.html", {"ls": ls})
+        return render(request, "main/list.html", {"ls": ls})
+    return render(request, "main/view.html", {})
 
 
 def home(request):
@@ -42,8 +44,14 @@ def create(request):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+            request.user.todolist.add(t)
 
         return HttpResponseRedirect("/%i" % t.id)
+
     else:
         form = CreateNewList()
     return render(request, "main/create.html", {"form": form})
+
+
+def view(request):
+    return render(request, "main/view.html", {})
